a60c9d86ac7666d2fe2c613c960ac5949bb21cfd
Tom Bremer <tom@tbremer.com>
2020-01-20 20:11:57 -0600

    get auth flow working

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.hello = functions.https.onRequest((request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({ hello: 'world ', now: new Date() }));
+});
