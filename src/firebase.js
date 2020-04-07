ab736ca00163bfd835bd3778de784c9114efed83
Tom Bremer <tom@tbremer.com>
2020-04-06 19:01:03 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,7 @@ import 'firebase/auth';
 import 'firebase/app';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyB0gEyh50EO1eBcn0cXTVVj4vfkXjsoT6c',
-  authDomain: 'dungeon-ddf25.firebaseapp.com',
-  databaseURL: 'https://dungeon-ddf25.firebaseio.com',
-  projectId: 'dungeon-ddf25',
-  storageBucket: 'dungeon-ddf25.appspot.com',
-  messagingSenderId: '191383226146',
-  appId: '1:191383226146:web:c3dab63b0728cc8126077b',
-};
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
